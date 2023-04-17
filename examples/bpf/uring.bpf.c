5ff0b8f00e7d207ccff56fc1e0f3fbcf75148c86
mithrao <c001hewanning@qq.com>
2023-04-17 10:37:51 +0800

    update bpf/

--- examples/bpf/uring.bpf.c
+++ examples/bpf/uring.bpf.c
@@ -4,56 +4,30 @@
 #include "uring.h"
 
 static inline void io_uring_prep_rw(int op, struct io_uring_sqe *sqe, int fd,
-                            const void *addr, unsigned len, __u64 offset)
+				    const void *addr, unsigned len,
+				    __u64 offset)
 {
-    sqe->opcode = op;
-    sqe->flags = 0;
-    sqe->ioprio = 0;
-    sqe->fd = fd;
-    sqe->off = offset;
-    sqe->addr = (unsigned long) addr;
-    sqe->len = len;
-    sqe->rw_flags = 0;
-    sqe->user_data = 0;
-    sqe->__pad2[0] = sqe->__pad2[1] = sqe->__pad2[2] = 0;
+	sqe->opcode = op;
+	sqe->flags = 0;
+	sqe->ioprio = 0;
+	sqe->fd = fd;
+	sqe->off = offset;
+	sqe->addr = (unsigned long) addr;
+	sqe->len = len;
+	sqe->rw_flags = 0;
+	sqe->user_data = 0;
+	sqe->__pad2[0] = sqe->__pad2[1] = sqe->__pad2[2] = 0;
 }
 
 static inline void io_uring_prep_nop(struct io_uring_sqe *sqe)
 {
-    io_uring_prep_rw(IORING_OP_NOP, sqe, -1, 0, 0, 0);
+	io_uring_prep_rw(IORING_OP_NOP, sqe, -1, 0, 0, 0);
 }
 
-/* test customized iouring bpf calls */
-/** iouring_queue_sqe (fs/io_uring.c/io_bpf_queue_sqe)
- * struct io_bpf_ctx *          bpf_ctx,
- * const struct io_uring_sqe *  sqe,
- * u32                          sqe_len
-*/
-static long (*iouring_queue_sqe) (void *ctx, struct io_uring_sqe *sqe, u32) = (void *) 164;
-
-/** iouring_emit_cqe (fs/io_uring.c/io_bpf_emit_cqe)
- * struct io_bpf_ctx *  bpf_ctx
- * u32                  cq_idx
- * u64                  user_data
- * s32                  res
- * u32                  flags
-*/
-static long (*iouring_emit_cqe) (void *ctx, u32 cq, u64 data, u32 res, u32 flags) = (void *) 165;
-
-/** iouring_reap_cqe (fs/io_uring.c/io_bpf_reap_cqe)
- * struct io_bpf_ctx * 	 bpf_ctx
- * u32				 	 cq_idx
- * struct io_uring_cqe * cqe_out
- * u32  				 cqe_len
-*/
-static long (*iouring_reap_cqe) (void *ctx, u32 cq, struct io_uring_cqe *cqe, u32) = (void *) 166;
-
-/** iouring_bpf_copy_to_user (kernel/bpf/helpers.c/bpf_copy_to_user)
- * void __user *    user_ptr
- * const void *     src
- * u32              size
-*/
-// static long (*iouring_bpf_copy_to_user) (void *ctx, const void *src, __u32 size) = (void *) 167;
+static long (*iouring_queue_sqe)(void *ctx, struct io_uring_sqe *sqe, u32) = (void *) 164;
+static long (*iouring_emit_cqe)(void *ctx, u32 cq, u64 data, u32 res, u32 flags) = (void *) 165;
+static long (*iouring_reap_cqe)(void *ctx, u32 cq, struct io_uring_cqe *cqe, u32) = (void *) 166;
+static long (*bpf_copy_to_user)(void *user_ptr, const void *src, __u32 size) = (void *) 167;
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -62,189 +36,204 @@ struct {
     __type(value, unsigned long);
 } arr SEC(".maps");
 
-#define ARR_SLOT        0
-#define REENTER_SLOT    10
+#define ARR_SLOT 0
+#define REENTER_SLOT 10
 
 static void writev(u32 kv, unsigned long v)
 {
-    unsigned long *val = bpf_map_lookup_elem(&arr, &kv);
-    if (val)
-        *val = v;
+	unsigned long *val = bpf_map_lookup_elem(&arr, &kv);
+
+	if (val)
+		*val = v;
 }
 
 static unsigned long readv(u32 kv)
 {
-    unsigned long *val = bpf_map_lookup_elem(&arr, &kv);
-    return val ? *val : -1UL;
+	unsigned long *val = bpf_map_lookup_elem(&arr, &kv);
+
+	return val ? *val : -1UL;
 }
 
-SEC("iouring.s/")
-int test(struct io_bpf_ctx *bpf_ctx)
+/*
+ * just a set of use examples for features
+ */
+SEC("iouring")
+int test(struct io_uring_bpf_ctx *ctx)
 {
-    struct io_uring_bpf_ctx *ctx = &bpf_ctx->u;
-    struct io_uring_sqe sqe;
-    struct io_uring_cqe cqe = {};
-    u32 key = 0;
-    long *val;
-    int ret, cq_idx = 1;
-    unsigned long secret, f1;
-    __u32 vvv;
-    u64 *uptr;
+	struct io_uring_sqe sqe;
+	struct io_uring_cqe cqe = {};
+	u32 key = 0;
+	long *val;
+	int ret, cq_idx = 1;
+	unsigned long secret, f1;
+	__u32 vvv;
+	u64 *uptr;
 
-    /* make sure we don't repeat it twice */
-    if (readv(REENTER_SLOT))
-        return 0;
-    writev(REENTER_SLOT, 1);
+	/* make sure we don't repeat it twice */
+	if (readv(REENTER_SLOT))
+		return 0;
+	writev(REENTER_SLOT, 1);
 
-    /* just write some values */
-    writev(ARR_SLOT, 11);
+	// just write some value
+	writev(ARR_SLOT, 11);
 
-    /* emit CQE to the main CQ */
-    iouring_emit_cqe(bpf_ctx, 0, 3, 13, 0);
+	// emit CQE to the main CQ
+	iouring_emit_cqe(ctx, 0, 3, 13, 0);
 
-    /* emit 2 CQEs to a second CQ and reap them */
-    iouring_emit_cqe(bpf_ctx, cq_idx, 4, 17, 0);
-    iouring_emit_cqe(bpf_ctx, cq_idx, 5, 19, 0);
-    ret = iouring_reap_cqe(bpf_ctx, cq_idx, &cqe, sizeof(cqe));
-    writev(ARR_SLOT + 1, ret ? ret : cqe.user_data);
-    ret = iouring_reap_cqe(bpf_ctx, cq_idx, &cqe, sizeof(cqe));
-    writev(ARR_SLOT + 2, ret ? ret : cqe.user_data);
+	// emit 2 CQEs to a second CQ and reap them
+	iouring_emit_cqe(ctx, cq_idx, 4, 17, 0);
+	iouring_emit_cqe(ctx, cq_idx, 5, 19, 0);
+	ret = iouring_reap_cqe(ctx, cq_idx, &cqe, sizeof(cqe));
+	writev(ARR_SLOT + 1, ret ? ret : cqe.user_data);
+	ret = iouring_reap_cqe(ctx, cq_idx, &cqe, sizeof(cqe));
+	writev(ARR_SLOT + 2, ret ? ret : cqe.user_data);
 
-    /* submit nop SQE */
-    io_uring_prep_nop(&sqe);
-    sqe.user_data = 2;
-    sqe.flags = 0;
-    ret = iouring_queue_sqe(bpf_ctx, &sqe, sizeof(sqe));
-    writev(ARR_SLOT + 3, ret < 0 ? ret : 21);
+	// submit nop SQE
+	io_uring_prep_nop(&sqe);
+	sqe.user_data = 2;
+	sqe.flags = 0;
+	ret = iouring_queue_sqe(ctx, &sqe, sizeof(sqe));
+	writev(ARR_SLOT + 3, ret < 0 ? ret : 21);
 
-    /* write back user_data */
-    writev(ARR_SLOT + 4, ctx->user_data);
+	// write back user_data
+	writev(ARR_SLOT + 4, sqe.user_data);
 
-    /* demo for reading from userspace */
-    uptr = (u64 *)(unsigned long) ctx->user_data;
-    bpf_copy_from_user(&secret, sizeof(secret), uptr);
-    writev(ARR_SLOT + 5, secret);
+	// demo for reading from userspace
+	uptr = (u64 *)(unsigned long)ctx->user_data;
+	bpf_copy_from_user(&secret, sizeof(secret), uptr);
+	writev(ARR_SLOT + 5, secret);
 
-    /* copy to userspace */
-    // secret = 31;
-    // bpf_copy_to_user(uptr, &secret, sizeof(secret));
+	// copy to userspace
+	secret = 31;
+	bpf_copy_to_user(uptr, &secret, sizeof(secret));
 
-    ctx->wait_idx = 0;
-    ctx->wait_nr = 1;
-
-    return 0;
+	ctx->wait_idx = 0;
+	ctx->wait_nr = 1;
+	return 0;
 }
 
 static inline void io_uring_prep_timeout(struct io_uring_sqe *sqe,
-                                        struct __kernel_timespec *ts,
-                                        unsigned count, unsigned flags)
+					 struct __kernel_timespec *ts,
+					 unsigned count, unsigned flags)
 {
-    io_uring_prep_rw(IORING_OP_TIMEOUT, sqe, -1, ts, 1, count);
-    sqe->timeout_flags = flags;
+	io_uring_prep_rw(IORING_OP_TIMEOUT, sqe, -1, ts, 1, count);
+	sqe->timeout_flags = flags;
 }
 
-SEC("iouring.s/")
-int counting(struct io_bpf_ctx *bpf_ctx)
+/*
+ * ticks-react N times using timeout reqs and CQ waiting
+ */
+SEC("iouring")
+int counting(struct io_uring_bpf_ctx *ctx)
 {
-    struct io_uring_bpf_ctx *ctx = &bpf_ctx->u;
-    struct counting_ctx *uctx = (void *)(unsigned long)ctx->user_data;
-    struct io_uring_sqe sqe;
-    struct io_uring_cqe cqe;
-    unsigned long v = readv(0);
-    unsigned int cq_idx = 1;
+	struct counting_ctx *uctx = (void *)(unsigned long)ctx->user_data;
+	struct io_uring_sqe sqe;
+	struct io_uring_cqe cqe;
+	unsigned long v = readv(0);
+	unsigned int cq_idx = 1;
 
-    if (v > 10)
-        return 0;
-    writev(0, v + 1);
+	if (v > 10)
+		return 0;
+	writev(0, v + 1);
 
-    if (v != 0) {
-        int ret = iouring_reap_cqe(bpf_ctx, cq_idx, &cqe, sizeof(cqe));
-        writev(1, ret ? ret : cqe.user_data);
-    }
+	if (v != 0) {
+		int ret = iouring_reap_cqe(ctx, cq_idx, &cqe, sizeof(cqe));
+		writev(1, ret ? ret : cqe.user_data);
+		writev(2, ret ? ret : cqe.res);
+	}
 
-    io_uring_prep_timeout(&sqe, &uctx->ts, 0, 0);
-    sqe.user_data = 5;
-    sqe.cq_idx = cq_idx;
-    iouring_queue_sqe(bpf_ctx, &sqe, sizeof(sqe));
+	io_uring_prep_timeout(&sqe, &uctx->ts, 0, 0);
+	sqe.user_data = 5;
+	sqe.cq_idx = cq_idx;
+	iouring_queue_sqe(ctx, &sqe, sizeof(sqe));
 
-    ctx->wait_idx = cq_idx;
-    ctx->wait_nr = 1;
+	ctx->wait_idx = cq_idx;
+	ctx->wait_nr = 1;
 
-    return 0;
+	return 0;
 }
 
-SEC("iouring.s/")
-int pingpong(struct io_bpf_ctx *bpf_ctx)
+/*
+ * two BPF reqs do message-based communication by
+ * repeatedly writing a CQE to another program's CQ and
+ * waiting for a response
+ */
+SEC("iouring")
+int pingpong(struct io_uring_bpf_ctx *ctx)
 {
-    struct io_uring_bpf_ctx *ctx = &bpf_ctx->u;
-    struct ping_ctx *uctx = (void *)(unsigned long)ctx->user_data;
-    struct io_uring_sqe sqe;
-    struct io_uring_cqe cqe;
-    unsigned long v;
-    int idx, ret, iter;
-    int cq_idx2;
+	struct ping_ctx *uctx = (void *)(unsigned long)ctx->user_data;
+	struct io_uring_sqe sqe;
+	struct io_uring_cqe cqe;
+	unsigned long v;
+	int idx, ret, iter;
+	int cq_idx2;
 
-    bpf_copy_from_user(&idx, sizeof(idx), &uctx->idx);
-    if (!readv(idx)) {
-        writev(idx, 1);
+	// uctx = [0,1]
+	// idx = 0
+	bpf_copy_from_user(&idx, sizeof(idx), &uctx->idx);
+	if (!readv(idx)) {
+		writev(idx, 1);
 wait:
-        ctx->wait_idx = idx + 1;
-        ctx->wait_nr = 1;
-        return 0;
-    }
-    ret = iouring_reap_cqe(bpf_ctx, idx + 1, &cqe, sizeof(cqe));
-    iter = cqe.user_data;
-    cq_idx2 = (idx ^ 1) + 1;
-    iouring_emit_cqe(bpf_ctx, cq_idx2, iter + 1, 0, 0);
+		ctx->wait_idx = idx + 1;
+		ctx->wait_nr = 1;
+		return 0;
+	}
 
-    if (iter < 20)
-        goto wait;
-    writev(idx + 5, iter);
-    return 0;
+	ret = iouring_reap_cqe(ctx, idx + 1, &cqe, sizeof(cqe));
+	iter = cqe.user_data;
+	cq_idx2 = (idx ^ 1) + 1;
+	iouring_emit_cqe(ctx, cq_idx2, iter + 1, 0, 0);
+
+	if (iter < 20)
+		goto wait;
+	writev(5 + idx, iter);
+	return 0;
 }
 
 static inline void io_uring_prep_write(struct io_uring_sqe *sqe, int fd,
-                            const void *buf, unsigned nbytes, off_t offset)
+				       const void *buf, unsigned nbytes, off_t offset)
 {
-    io_uring_prep_rw(IORING_OP_WRITE, sqe, fd, buf, nbytes, offset);
+	io_uring_prep_rw(IORING_OP_WRITE, sqe, fd, buf, nbytes, offset);
 }
 
-#define IOSQE_FIXED_FILE        (1U << IOSQE_FIXED_FILE_BIT)
+#define IOSQE_FIXED_FILE	(1U << IOSQE_FIXED_FILE_BIT)
 
-SEC("iouring.s/")
-int write_file(struct io_bpf_ctx *bpf_ctx)
+/*
+ * BPF writes N bytes to a file keeping QD>1
+ */
+SEC("iouring")
+int write_file(struct io_uring_bpf_ctx *ctx)
 {
-    struct io_uring_bpf_ctx *ctx = &bpf_ctx->u;
-    const int off_idx = 1, infl_idx = 0;
-    struct io_uring_sqe sqe;
-    struct io_uring_cqe cqe;
-    void *buf = (void *)(unsigned long)ctx->user_data;
-    u64 ret, i, inflight = readv(infl_idx);
-    u64 cur_off = readv(off_idx);
+	const int off_idx = 1, infl_idx = 0;
+	struct io_uring_sqe sqe;
+	struct io_uring_cqe cqe;
+	void *buf = (void *)(unsigned long)ctx->user_data;
+	u64 ret, i, inflight = readv(infl_idx);
+	u64 cur_off = readv(off_idx);
 
-    for (i = 0; i < FILL_QD; i++) {
-        if (inflight) {
-            ret = iouring_reap_cqe(bpf_ctx, 1, &cqe, sizeof(cqe));
-            inflight--;
-        }
-    }
-    for (i = 0; i < FILL_QD; i++) {
-        if (inflight >= FILL_QD || cur_off >= FILL_BLOCKS)
-            break;
-        io_uring_prep_write(&sqe, 0, buf, FILL_BLOCK_SIZE, 
-                            cur_off * FILL_BLOCK_SIZE);
-        sqe.flags = IOSQE_FIXED_FILE;
-        sqe.cq_idx = 1;
-        ret = iouring_queue_sqe(bpf_ctx, &sqe, sizeof(sqe));
-        inflight++;
-        cur_off++;
-    }
-    
-    writev(off_idx, cur_off);
-    writev(infl_idx, inflight);
-    ctx->wait_idx = 1;
-    ctx->wait_nr = inflight;
-    return 0;
+	for (i = 0; i < FILL_QD; i++) {
+		if (inflight) {
+			ret = iouring_reap_cqe(ctx, 1, &cqe, sizeof(cqe));
+			inflight--;
+		}
+	}
+	for (i = 0; i < FILL_QD; i++) {
+		if (inflight >= FILL_QD || cur_off >= FILL_BLOCKS)
+			break;
+		io_uring_prep_write(&sqe, 0, buf, FILL_BLOCK_SIZE,
+				    cur_off * FILL_BLOCK_SIZE);
+		sqe.flags = IOSQE_FIXED_FILE;
+		sqe.cq_idx = 1;
+		ret = iouring_queue_sqe(ctx, &sqe, sizeof(sqe));
+		inflight++;
+		cur_off++;
+	}
+
+	writev(off_idx, cur_off);
+	writev(infl_idx, inflight);
+	ctx->wait_idx = 1;
+	ctx->wait_nr = inflight;
+	return 0;
 }
 
 char LICENSE[] SEC("license") = "GPL";
--- examples/bpf/uring.c
+++ examples/bpf/uring.c
@@ -17,259 +17,244 @@
 
 static inline void io_uring_prep_bpf(struct io_uring_sqe *sqe, unsigned idx)
 {
-    io_uring_prep_nop(sqe);
-    sqe->off = idx;
-    sqe->opcode = IORING_OP_BPF;
+	io_uring_prep_nop(sqe);
+	sqe->off = idx;
+	sqe->opcode = IORING_OP_BPF;
 }
 
 static void ring_prep(struct io_uring *ring, struct uring_bpf **pobj)
 {
-    struct uring_bpf *payload;
-    struct io_uring_params param;
-    __u32 cq_sizes[2] = {128, 128};
-    int prog_fds[4];
-    int ret;
+	struct uring_bpf *obj;
+	struct io_uring_params param;
+	__u32 cq_sizes[2] = {128, 128};
+	int ret, prog_fds[4];
 
-    /* 1 additional CQ, 2 in total */
-    memset(&param, 0, sizeof(param));
-    param.nr_cq = ARRAY_SIZE(cq_sizes);
-    param.cq_sizes = (__u64) (unsigned long) cq_sizes;
-    ret = io_uring_queue_init_params(8, ring, &param);
-    if (ret) {
-        fprintf(stderr, "ring setup fialed: %d\n", ret);
-        exit(1);
-    }
-
-    payload = uring_bpf__open();
-	if (!payload) {
-		fprintf(stderr, "failed to open and/or load BPF object\n");
+	memset(&param, 0, sizeof(param));
+	param.nr_cq = ARRAY_SIZE(cq_sizes);
+	param.cq_sizes = (__u64)(unsigned long)cq_sizes;
+	ret = io_uring_queue_init_params(8, ring, &param);
+	if (ret) {
+		fprintf(stderr, "ring setup failed: %d\n", ret);
 		exit(1);
 	}
 
-	ret = uring_bpf__load(payload);
+	obj = uring_bpf__open();
+	if (!obj) {
+		fprintf(stderr, "failed to open and/or load BPF object\n");
+		exit(1);
+	}
+	ret = uring_bpf__load(obj);
 	if (ret) {
 		fprintf(stderr, "failed to load BPF object: %d\n", ret);
 		exit(1);
 	}
 
-    prog_fds[0] = bpf_program__fd(payload->progs.test);
-    prog_fds[1] = bpf_program__fd(payload->progs.counting);
-    prog_fds[2] = bpf_program__fd(payload->progs.pingpong);
-    prog_fds[3] = bpf_program__fd(payload->progs.write_file);
-    ret = __sys_io_uring_register(ring->ring_fd, IORING_REGISTER_BPF,
-                                    prog_fds, ARRAY_SIZE(prog_fds));
-
-    
-    if (ret < 0) {
-        fprintf(stderr, "bpf prog register failed %i\n", ret);
+	prog_fds[0] = bpf_program__fd(obj->progs.test);
+	prog_fds[1] = bpf_program__fd(obj->progs.counting);
+	prog_fds[2] = bpf_program__fd(obj->progs.pingpong);
+	prog_fds[3] = bpf_program__fd(obj->progs.write_file);
+	ret = __sys_io_uring_register(ring->ring_fd, IORING_REGISTER_BPF,
+					prog_fds, ARRAY_SIZE(prog_fds));
+	if (ret < 0) {
+		fprintf(stderr, "bpf prog register failed %i\n", ret);
 		exit(1);
-    }
-    *pobj = payload;
+	}
+	*pobj = obj;
 }
 
 static void print_map(int map_fd, int limit)
 {
-    int i;
-    for (i = 0; i < limit; i++) {
-        unsigned long cnt;
-        __u32 key = i;
-        assert(bpf_map_lookup_elem(map_fd, &key, &cnt) == 0);
-        fprintf(stderr, "%lu ", cnt);
-    }
+	int i;
+
+	for (i = 0; i < limit; i++) {
+		unsigned long cnt;
+		__u32 key = i;
+
+		assert(bpf_map_lookup_elem(map_fd, &key, &cnt) == 0);
+		fprintf(stderr, "%lu ", cnt);
+	}
 	fprintf(stderr, "\n");
 }
 
-/** example 1
- * bpf writing to userspace: `copy_to_user`
- * bpf reading from userspace: `copy_from_user` 
- * bpf waiting on CQ: `io_uring_wait_cqe`
- */
 static int test1(void)
 {
-    struct io_uring ring;
-	struct io_uring_sqe *sqe;
+	struct io_uring ring;
 	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
 	struct uring_bpf *obj;
 	int ret;
 	unsigned long secret = 29;
 
-    ring_prep(&ring, &obj);
+	ring_prep(&ring, &obj);
 
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_bpf(sqe, 0);
-    sqe->user_data = (__u64)(unsigned long) &secret;
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_bpf(sqe, 0);
+	sqe->user_data = (__u64)(unsigned long)&secret;
 
-    ret = io_uring_submit(&ring);
-    assert(ret == 1);
+	ret = io_uring_submit(&ring);
+	assert(ret == 1);
 
-    sleep(1);
-    io_uring_wait_cqe(&ring, &cqe);
-    while (1) {
-        ret = io_uring_peek_cqe(&ring, &cqe);
-        if (ret == -EAGAIN)
-            break;
-        assert(ret == 0);
-        fprintf(stderr, "CQE user_data %lu, res %i flags %u\n",
-            (unsigned long) cqe->user_data,
-            (int) cqe->res, (unsigned) cqe->flags);
-        io_uring_cqe_seen(&ring, cqe);
-    }
+	sleep(1);
+	io_uring_wait_cqe(&ring, &cqe);
+	while (1) {
+		ret = io_uring_peek_cqe(&ring, &cqe);
+		if (ret == -EAGAIN)
+			break;
 
-    print_map(bpf_map__fd(obj->maps.arr), 10);
-    fprintf(stderr, "new secret %lu\n", secret);
-    uring_bpf__destroy(obj);
-    io_uring_queue_exit(&ring);
-    return 0;
+		assert(ret == 0);
+		fprintf(stderr, "CQE user_data %lu, res %i flags %u\n",
+			(unsigned long)cqe->user_data,
+			(int)cqe->res, (unsigned)cqe->flags);
+		io_uring_cqe_seen(&ring, cqe);
+	}
+
+	print_map(bpf_map__fd(obj->maps.arr), 10);
+	fprintf(stderr, "new secret %lu\n", secret);
+	uring_bpf__destroy(obj);
+	io_uring_queue_exit(&ring);
+	return 0;
 }
 
-/** example 2
- * wait for N timeout requests 
- */
 static int test2(void)
 {
-    struct counting_ctx b = {
-        .ts = { 
-            .tv_nsec = 200000000, 
-        }
-    };
-    struct io_uring ring;
-    struct io_uring_sqe *sqe;
-    struct io_uring_cqe *cqe;
-    struct uring_bpf *obj;
-    int ret;
+	struct counting_ctx b = { .ts = { .tv_nsec = 200000000, } };
+	struct io_uring ring;
+	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
+	struct uring_bpf *obj;
+	int ret;
 
-    ring_prep(&ring, &obj);
+	ring_prep(&ring, &obj);
 
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_bpf(sqe, 1);
-    sqe->user_data = (__u64)(unsigned long) &b;
-    ret = io_uring_submit(&ring);
-    assert(ret == 1);
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_bpf(sqe, 1);
+	sqe->user_data = (__u64)(unsigned long)&b;
+	fprintf(stderr, "sqe->user_data %lu\n", (unsigned long)sqe->user_data);
+	ret = io_uring_submit(&ring);
+	assert(ret == 1);
 
-    ret = io_uring_wait_cqe(&ring, &cqe);
-    assert(!ret);
-    fprintf(stderr, "ret %i, udata %lu\n", cqe->res, (unsigned long) cqe->user_data);
-    io_uring_cqe_seen(&ring, cqe);
+	ret = io_uring_wait_cqe(&ring, &cqe);
+	assert(!ret);
+	fprintf(stderr, "res %i, udata %lu\n", cqe->res, (unsigned long)cqe->user_data);
+	io_uring_cqe_seen(&ring, cqe);
 
-    print_map(bpf_map__fd(obj->maps.arr), 10);
-    uring_bpf__destroy(obj);
-    io_uring_queue_exit(&ring);
-    
-    return 0;
+	print_map(bpf_map__fd(obj->maps.arr), 10);
+	uring_bpf__destroy(obj);
+	io_uring_queue_exit(&ring);
+	return 0;
 }
 
-/** example 3
- * ping pong CQEs b/w 2 BPF requests 
- */
 static int test3(void)
 {
-    struct ping_ctx uctx[2];
-    struct io_uring ring;
-    struct io_uring_sqe *sqe;
-    struct io_uring_cqe *cqe;
-    struct uring_bpf *obj;
-    int i, ret;
+	struct ping_ctx uctx[2];
+	struct io_uring ring;
+	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
+	struct uring_bpf *obj;
+	int i, ret;
 
-    ring_prep(&ring, &obj);
-    uctx[0].idx = 0;
-    uctx[1].idx = 1;
+	ring_prep(&ring, &obj);
+	uctx[0].idx = 0;
+	uctx[1].idx = 1;
 
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_bpf(sqe, 2);
-    sqe->user_data = (__u64)(unsigned long) &uctx[0];
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_bpf(sqe, 2);
+	sqe->user_data = (__u64)(unsigned long)&uctx[0];
 
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_bpf(sqe, 2);
-    sqe->user_data = (__u64)(unsigned long) &uctx[1];
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_bpf(sqe, 2);
+	sqe->user_data = (__u64)(unsigned long)&uctx[1];
 
-    /* kick off the first bpf */
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_nop(sqe);
-    sqe->user_data = 0; /* start from 0 */
-    sqe->cq_idx = 1;
+	// kick off the first bpf
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_nop(sqe);
+	sqe->user_data = 0; // start from 0
+	sqe->cq_idx = 1;
 
-    ret = io_uring_submit(&ring);
-    assert(ret == 3);
+	ret = io_uring_submit(&ring);
+	assert(ret == 3);
 
-    /* wait for bpf's CQE */
-    for (i = 0; i < 2; i++) {
-        ret = io_uring_wait_cqe(&ring, &cqe);
-        assert(!ret);
-        fprintf(stderr, "ret %i, udata %lu\n", cqe->res, (unsigned long)cqe->user_data);
-        io_uring_cqe_seen(&ring, cqe);
-    }
+	// wait for bpf's CQEs
+	for (i = 0; i < 2; i++) {
+		ret = io_uring_wait_cqe(&ring, &cqe);
+		assert(!ret);
+		fprintf(stderr, "res %i, udata %lu\n", cqe->res, (unsigned long)cqe->user_data);
+		io_uring_cqe_seen(&ring, cqe);
+	}
 
-    print_map(bpf_map__fd(obj->maps.arr), 10);
-    uring_bpf__destroy(obj);
-    io_uring_queue_exit(&ring);
-    return 0;
+	print_map(bpf_map__fd(obj->maps.arr), 10);
+	uring_bpf__destroy(obj);
+	io_uring_queue_exit(&ring);
+	return 0;
 }
 
 static char verify_buf[FILL_BLOCK_SIZE];
-/** example 4
- * bpf writing to file with QD > 1 
- */
+
 static int test4(void)
 {
-    struct io_uring ring;
+	struct io_uring ring;
 	struct io_uring_cqe *cqe;
 	struct io_uring_sqe *sqe;
 	struct uring_bpf *obj;
 	int i, j, ret, fd;
-    char filename[] = "./.tmp/bpf_file_XXXXXX";
+	char filename[] = "./skel/bpf_file_XXXXXX";
 	char pattern = 0xae;
-    void *mem;
+	void *mem;
 
-    ret = posix_memalign(&mem, 4096, FILL_FSIZE);
-    assert(!ret);
-    memset(mem, pattern, FILL_FSIZE);
+	ret = posix_memalign(&mem, 4096, FILL_FSIZE);
+	assert(!ret);
+	memset(mem, pattern, FILL_FSIZE);
 
-    fd = mkstemp(filename);
-    if (fd < 0) {
-        fprintf(stderr, "mkstemp failed\n");
-        return 1;
-    }
-    unlink(filename);
+	fd = mkstemp(filename);
+	if (fd < 0) {
+		fprintf(stderr, "mkstemp failed\n");
+		return 1;
+	}
+	unlink(filename);
 
-    ring_prep(&ring, &obj);
-    ret = io_uring_register_files(&ring, &fd, 1);
-    assert(!ret);
+	ring_prep(&ring, &obj);
+	ret = io_uring_register_files(&ring, &fd, 1);
+	assert(!ret);
 
-    sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_bpf(sqe, 3);
-    sqe->user_data = (__u64)(unsigned long) mem;
-    ret = io_uring_submit(&ring);
-    assert(ret == 1);
+	sqe = io_uring_get_sqe(&ring);
+	io_uring_prep_bpf(sqe, 3);
+	sqe->user_data = (__u64)(unsigned long)mem;
+	ret = io_uring_submit(&ring);
+	assert(ret == 1);
 
-    ret = io_uring_wait_cqe(&ring, &cqe);
-    assert(!ret);
-    fprintf(stderr, "ret %i, udata %lu\n", cqe->res, (unsigned long) cqe->user_data);
-    io_uring_cqe_seen(&ring, cqe);
+	ret = io_uring_wait_cqe(&ring, &cqe);
+	assert(!ret);
+	fprintf(stderr, "ret %i, udata %lu\n", cqe->res, (unsigned long)cqe->user_data);
+	io_uring_cqe_seen(&ring, cqe);
 
-    print_map(bpf_map__fd(obj->maps.arr), 10);
-    uring_bpf__destroy(obj);
-    io_uring_queue_exit(&ring);
-    free(mem);
+	print_map(bpf_map__fd(obj->maps.arr), 10);
+	uring_bpf__destroy(obj);
+	io_uring_queue_exit(&ring);
+	free(mem);
 
-    for (i = 0; i < FILL_BLOCKS; i++) {
-        ret = read(fd, verify_buf, FILL_BLOCK_SIZE);
-        assert(ret == FILL_BLOCK_SIZE);
-        for (j = 0; j < FILL_BLOCK_SIZE; j++) {
-            assert(verify_buf[j] == pattern);
-        }
-    }
-    return 0;
+	for (i = 0; i < FILL_BLOCKS; i++) {
+		ret = read(fd, verify_buf, FILL_BLOCK_SIZE);
+		assert(ret == FILL_BLOCK_SIZE);
+		for (j = 0; j < FILL_BLOCK_SIZE; j++)
+			assert(verify_buf[i] == pattern);
+	}
+
+	return 0;
 }
 
-int main(int argc, char const *argv[])
+int main(int arg, char **argv)
 {
-    fprintf(stderr, "test1()============\n");
-    test1();
-    fprintf(stderr, "test2()============\n");
-    test2();
-    fprintf(stderr, "test3()============\n");
-    test3();
-    fprintf(stderr, "test4()============\n");
-    test4();
-    return 0;
+	fprintf(stderr, "test1() ============\n");
+	test1();
+
+	fprintf(stderr, "\ntest2() ============\n");
+	test2();
+
+	fprintf(stderr, "\ntest3() ============\n");
+	test3();
+
+	fprintf(stderr, "\ntest4() ============\n");
+	test4();
+
+	return 0;
 }
